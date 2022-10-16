01ee186512dfa89bbb806339e09b6656e3195b58
marbleVHS <goldhemming@gmail.com>
2022-10-16 04:54:26 +0300

    OfflineFeedTestCase

--- app/src/androidTest/java/com/mindorks/framework/mvvm/tests/OfflineFeedTestCase.java
+++ app/src/androidTest/java/com/mindorks/framework/mvvm/tests/OfflineFeedTestCase.java
@@ -7,6 +7,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import com.mindorks.framework.mvvm.pageObjects.DrawerPage;
 import com.mindorks.framework.mvvm.pageObjects.LoginPage;
@@ -21,11 +22,9 @@ import com.mindorks.framework.mvvm.ui.login.LoginActivity;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 
-/**
- * I couldn't find how to turn off/on internet using only Espresso,
- * but it could be done with Kaspresso device.network interface.
- */
+
 public class OfflineFeedTestCase {
 
     @Rule
@@ -33,42 +32,55 @@ public class OfflineFeedTestCase {
 
     @Before
     public void setUp() throws Throwable {
+        turnInternetOn();
         // navigating to Feed screen from entrypoint
         LoginPage.loginWithTestCredentials();
-        // TODO: Turn Off Internet Here
+        turnInternetOff();
         MainPage.openDrawer();
         DrawerPage.openFeed();
     }
 
     @After
     public void tearDown() throws Throwable {
+        turnInternetOn();
         FeedPage.navigateUp();
         MainPage.openDrawer();
         DrawerPage.logout();
     }
 
-
+    @Test
     public void checkOfflineBlogTab() throws Throwable {
         FeedPage.openBlogTab();
         onView(BlogFeedTabPage.ivEmpty).check(matches(isDisplayed()));
         onView(BlogFeedTabPage.tvMessage).check(matches(isDisplayed()));
         onView(BlogFeedTabPage.btnRetry).check(matches(isDisplayed()));
-        // TODO: Turn On Internet here
+        turnInternetOn();
         BlogFeedTabPage.clickRetry();
         FlakyActionExecutor.checkOnView(BlogFeedTabPage.rvBlog, matches(hasDescendant(withId(R.id.coverImageView))));
 
     }
 
-
+    @Test
     public void checkOfflineOpenSourceTab() throws Throwable {
         FeedPage.openOpensourceTab();
         onView(OpenSourceFeedTabPage.ivEmpty).check(matches(isDisplayed()));
         onView(OpenSourceFeedTabPage.tvMessage).check(matches(isDisplayed()));
         onView(OpenSourceFeedTabPage.btnRetry).check(matches(isDisplayed()));
-        // TODO: Turn On Internet here
+        turnInternetOn();
         OpenSourceFeedTabPage.clickRetry();
         FlakyActionExecutor.checkOnView(OpenSourceFeedTabPage.rvOpensource, matches(hasDescendant(withId(R.id.coverImageView))));
     }
 
+    private void turnInternetOn() throws InterruptedException {
+        InstrumentationRegistry.getInstrumentation().getUiAutomation().executeShellCommand("svc wifi enable");
+        InstrumentationRegistry.getInstrumentation().getUiAutomation().executeShellCommand("svc data enable");
+        Thread.sleep(500L);
+    }
+
+    private void turnInternetOff() throws InterruptedException {
+        InstrumentationRegistry.getInstrumentation().getUiAutomation().executeShellCommand("svc wifi disable");
+        InstrumentationRegistry.getInstrumentation().getUiAutomation().executeShellCommand("svc data disable");
+        Thread.sleep(500L);
+    }
 
 }
